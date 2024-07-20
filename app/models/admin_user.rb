4ee1f344a7d9df27791014c3e3d034dcfa13da7a
Peixuan Sun <psun4@academic.rrc.ca>
2024-07-20 10:38:29 +0800

    Fix active admin dash

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,9 @@
 class AdminUser < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :rememberable, :validatable
+
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["email"]
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["description", "title"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
