f5b179d632cbfbab5f3fc21c23fde5c340f403b4
Peixuan Sun <psun4@academic.rrc.ca>
2024-07-21 15:30:41 +0800

    Add seed to populate products and catgories

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+if Rails.env.development?
+  # Create Admin user
+  AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if AdminUser.find_by(email:'admin@example.com').blank?
+
+  # Create 4 products
+  4.times do |i|
+    Category.create!(name: Faker::Book.genre)
+  end
+
+  # Create 100 products
+  Category.all.each do |c|
+    25.times do |i|
+      c.products.create!(
+        title: Faker::Book.title,
+        description: Faker::Lorem.paragraph,
+        author: Faker::Book.author,
+        price: Faker::Number.decimal(l_digits: 2),
+        stock: Faker::Number.number(digits: 2))
+    end
+  end
+end
