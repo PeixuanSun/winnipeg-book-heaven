3e9c41a1232c35c6cf9c649ee7f642c7392d474c
Peixuan Sun <psun4@academic.rrc.ca>
2024-07-26 00:06:19 +0800

    Add validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,16 @@
 class Product < ApplicationRecord
   has_many :product_categories
   has_many :categories, through: :product_categories
+  has_one_attached :image
+
+  validates :title, :author, :description, :price, :stock,  presence: true
+  validates :price, :stock, numericality: { greater_than_or_equal_to: 0 }
+  validates :title, uniqueness: true
 
   private
 
   def self.ransackable_attributes(auth_object = nil)
-    ["author", "description", "id", "id_value", "price", "stock", "title"]
+    ["author", "description", "id", "id_value", "price", "stock", "title","image_attachment_id","image_blob_id"]
   end
 
   def self.ransackable_associations(auth_object = nil)
